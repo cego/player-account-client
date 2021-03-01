8dc94558e5258fc7acc0a04159ee32fc55c8f8d0
Thomas Wogelius <thwo@cego.dk>
2021-03-01 09:55:43 +0100

    Resolved discussions

--- src/PlayerAccount/Enumerations/Incidents.php
+++ src/PlayerAccount/Enumerations/Incidents.php
@@ -4,21 +4,21 @@ namespace Cego\PlayerAccount\Enumerations;
 
 class Incidents
 {
-    // Brugeren har en spilscannerscore på over 60, har en alder på 18 - 25 og og skal kontaktes via e-mail.
+    // Game scanner score > 60, age 18-25. Contact via email.
     const AGE_BETWEEN_18_AND_25 = 'AgeBetween18And25';
 
-    // Kunden skal overvåges af support, modtage et supportopkald og må ikke modtage reklame via e-mail eller sms.
+    // Must be monitored by support, receive support call. May not receive email or SMS advertising.
     const CUSTOMER_CARE = 'CustomerCare';
 
-    // Brugeren har en spilscannerscore på over 70, samt et akkumuleret tab over 3 mdr. på 50.000 kr. - 99.999 kr og skal kontaktes via e-mail
-    const LOSS_ACCUMOLATED_OVER_THREE_MONTHS = 'LossAccumulatedOverThreeMonths';
+    // Game scanner score > 70, acc. loss of 50.000 - 99.999 DKK through 3 months. Contact via email.
+    const LOSS_ACCUMULATED_OVER_THREE_MONTHS = 'LossAccumulatedOverThreeMonths';
 
-    // Kunden har øget den månedlige indbetalingsgrænse til 10.000 SEK eller derover og skal overvåges af support, samt modtage et supportopkald, når indbetalingsgrænse er nået 8.000 SEK.
+    // Customer has increased their monthly deposit limit to >= 10.000 SEK. Must be monitored and receive support call, when 8.000 SEK has been deposited.
     const PAYMENT_LIMIT_INCREASED_MONTHLY = 'PaymentLimitIncreasedMonthly';
 
-    // Kunden har øget den månedlige indbetalingsgrænse til 10.000 SEK eller derover og skal overvåges af support, samt modtage et supportopkald, når indbetalingsgrænse er nået 8.000 SEK.
+    // Payment limit increased several times. Must be monitored by support, receive support call.
     const PAYMENT_LIMIT_INCREASED_SEVERAL_TIMES = 'PaymentLimitIncreasedSeveralTimes';
 
-    // Brugeren har en spilscannerscore på over 60, som er steget med 20-25 over den sidste måned og skal kontaktes via e-mail.
+    // Game scanner score > 60. Score increased by 20-25 points through the last month. Contact via email.
     const SCORE_INCREASED_FROM_20_TO_25_LAST_MONTH = 'ScoreIncreasedFrom20To25LastMonth';
 }

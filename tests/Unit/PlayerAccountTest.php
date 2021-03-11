4aa2e120abf601adb18833974f4c0a8b811d0328
Thomas Wogelius <thwo@cego.dk>
2021-03-11 15:02:47 +0100

    Added tests

---
+++ tests/Unit/PlayerAccountTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Unit;
+
+use Mockery;
+use Tests\TestCase;
+use Mockery\MockInterface;
+use Cego\PlayerAccount\PlayerAccount;
+use Cego\PlayerAccount\Enumerations\Endpoints;
+
+class PlayerAccountTest extends TestCase
+{
+    protected MockInterface $mock;
+
+    protected function setUp(): void
+    {
+        $this->mock = Mockery::mock(PlayerAccount::class)
+            ->makePartial()
+            ->shouldAllowMockingProtectedMethods();
+
+        parent::setUp();
+    }
+
+    /** @test */
+    public function incident()
+    {
+        // Assert
+        $this->mock->shouldReceive('postRequest')
+            ->once()
+            ->with(sprintf(Endpoints::INCIDENT, 1), ['type' => 'test']);
+
+        // Act
+        $this->mock->incident(1, 'test');
+    }
+
+    /** @test */
+    public function update_without_admin_user()
+    {
+        // Assert
+        $this->mock->shouldReceive('postRequest')
+            ->once()
+            ->with(sprintf(Endpoints::UPDATE, 1), ['attribute' => 'newValue']);
+
+        // Act
+        $this->mock->update(1, ['attribute' => 'newValue']);
+    }
+
+    /** @test */
+    public function update_with_admin_user()
+    {
+        // Assert
+        $this->mock->shouldReceive('postRequest')
+            ->once()
+            ->with(sprintf(Endpoints::UPDATE, 1), ['attribute' => 'newValue', 'admin_user_id' => 2]);
+
+        // Act
+        $this->mock->update(1, ['attribute' => 'newValue'], 2);
+    }
+}

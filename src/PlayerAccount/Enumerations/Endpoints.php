e63e024a1d84e1f29621c1924f52d95d9f14a859
Thomas Wogelius <thwo@cego.dk>
2021-03-12 13:46:25 +0100

    Endpoint was still wrong

--- src/PlayerAccount/Enumerations/Endpoints.php
+++ src/PlayerAccount/Enumerations/Endpoints.php
@@ -5,5 +5,5 @@ namespace Cego\PlayerAccount\Enumerations;
 class Endpoints
 {
     const INCIDENT = 'api/v1/user/%s/incident';
-    const UPDATE = 'api/v1/user/%s/update';
+    const UPDATE = 'api/v1/user/%s';
 }

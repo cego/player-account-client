bb9c0b86fb89b2473deeefd772d0b2f29321e4d9
Thomas Wogelius <thwo@cego.dk>
2021-04-20 10:49:55 +0200

    Added support for addflag and removeflag endpoints

--- src/PlayerAccount/Enumerations/Endpoints.php
+++ src/PlayerAccount/Enumerations/Endpoints.php
@@ -4,6 +4,8 @@ namespace Cego\PlayerAccount\Enumerations;
 
 class Endpoints
 {
-    const INCIDENT = 'api/v1/user/%s/incident';
     const UPDATE = 'api/v1/user/%s';
+    const ADD_FLAG = 'api/v1/user/%s/flag/add';
+    const REMOVE_FLAG = 'api/v1/user/%s/flag/remove';
+    const INCIDENT = 'api/v1/user/%s/incident';
 }
